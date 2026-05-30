6f8bf9afe906eee3d0a217d7f139c28dfead2cbc
YoshikageKira425 <eneshalili425@gmail.com>
2026-05-30 11:10:47 +0200

    Made the design of chess.

---
+++ assets/design/chess.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="chess" tilewidth="176" tileheight="176" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../sprites/board/board_with_border_01.png" width="176" height="176"/>
+ </tile>
+ <tile id="1">
+  <image source="../sprites/white_pieces/pawn_white.png" width="12" height="16"/>
+ </tile>
+</tileset>
