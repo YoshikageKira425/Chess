c1428e8a1fe335e93165b48ab7d0714d9116e7e3
YoshikageKira425 <eneshalili425@gmail.com>
2026-06-19 18:25:42 +0200

    Added the bishop.

--- src/pieces/bishop.py
+++ src/pieces/bishop.py
@@ -1,6 +1,6 @@
 from .piece import Piece
 
-class Rook(Piece):
+class Bishop(Piece):
     def __init__(self, piece):
         super().__init__(piece)
 
@@ -8,19 +8,17 @@ class Rook(Piece):
         from_row, from_col = from_pos
         to_row, to_col = to_pos
 
-        if not (from_row == to_row or from_col == to_col):
+        if abs(to_row - from_row) != abs(to_col - from_col):
             return False
-
-        if from_row == to_row and from_col == to_col:
-            return False
-
-        row_step = 0 if from_row == to_row else (1 if to_row > from_row else -1)
-        col_step = 0 if from_col == to_col else (1 if to_col > from_col else -1)
+        
+        row_step = 1 if from_row < to_row else -1
+        col_step = 1 if from_col < to_col else -1
 
         cur_row, cur_col = from_row + row_step, from_col + col_step
         while (cur_row, cur_col) != (to_row, to_col):
             if board[cur_row][cur_col] is not None:
                 return False
+            
             cur_row += row_step
             cur_col += col_step
 
